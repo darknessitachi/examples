810d22c9a62290746426a9b6e4af87b2681a3fa7
nk3310 <nk3310@126.com>
2014-01-14 15:07:58 +0800

    测试 git@code.csdn.net:nk3310/examples.git（私有） 的第一次提交。

---
+++ groovy/DSL-Order.groovy
@@ -0,0 +1,60 @@
+// Listing 2.2 Order Processing DSL in Groovy
+
+package ch2.order
+
+class Order {
+  def security
+  def quantity
+  def limitPrice
+  def allOrNone
+  def value
+  def bs
+
+  def buy(su, closure) {
+    bs = 'Bought'
+    buy_sell(su, closure)
+  }
+
+  def sell(su, closure) {
+    bs = 'Sold'
+    buy_sell(su, closure)
+  }
+
+  private buy_sell(su, closure) {
+    security = su[0]
+    quantity = su[1]
+    closure()
+  }
+
+  def getTo() {
+    this
+  }
+}
+
+
+// Meta-programming hook to intercept non-existent method calls
+def methodMissing(String name, args) {
+  order.metaClass.getMetaProperty(name).setProperty(order, args)
+}
+
+def getNewOrder() {
+  order = new Order()
+}
+
+// Closure for inline valuation strategy specification
+def valueAs(closure) {
+  order.value = closure(order.quantity, order.limitPrice[0])
+}
+
+// groovy meta-programming technique that injects new method calls
+Integer.metaClass.getShares = { -> delegate }
+Integer.metaClass.of = { instrument ->  [instrument, delegate] }
+
+
+ord =
+newOrder.to.buy(100.shares.of('IBM')) {
+  limitPrice   300
+  allOrNone    true
+  valueAs      {qty, unitPrice -> qty * unitPrice - 500}
+}
+println "--------------> " + ord + "  <----------------"

8d84e0be8e9ed60e8c157319fb73e6e77ffbdadc
unknown <nk3310@126.com>
2014-06-30 10:18:30 +0800

    测试guava event的小例子。

---
+++ Guava/eventbus/src/main/java/org/chenj/test/Guava/eventbus/test1/TestEventBus.java
@@ -0,0 +1,19 @@
+package org.chenj.test.Guava.eventbus.test1;
+
+public class TestEventBus {
+    @Test
+    public void testReceiveEvent() throws Exception {
+
+        EventBus eventBus = new EventBus("test");
+        EventListener listener = new EventListener();
+
+        eventBus.register(listener);
+
+        eventBus.post(new TestEvent(200));
+        eventBus.post(new TestEvent(300));
+        eventBus.post(new TestEvent(400));
+
+        System.out.println("LastMessage:"+listener.getLastMessage());
+        ;
+    }
+}
